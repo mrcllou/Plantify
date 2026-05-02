38a83da1e63716a745e857733e4e1642e312c779
JesseH0630 <zxh0630@outlook.com>
2026-05-02 11:21:55 -0700

    Updated code structure
    Updated code structure to remove excessively long lines and different variables for changeable parameters.

--- API.java
+++ API.java
@@ -4,20 +4,28 @@ import java.io.IOException;
 
 public class API {
 	private final HttpClient client;
-	private final String url = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/83a29c21b009d77caa842ade2b3fd275/VIIRS_SNPP_NRT/-124.848974,45.543541,-116.915989,49.002494/1";
+	private final String MAP_KEY = "83a29c21b009d77caa842ade2b3fd275";
+	private final double MIN_LONGITUDE = -124.848974;
+	private final double MAX_LONGITUDE = -116.915989;
+	private final double MIN_LATITUDE = 45.543541;
+	private final double MAX_LATITUDE = 49.002494;
+
+	private String url;
 
 	public API() {
-		this.client = HttpClient.newHttpClient();
+		client = HttpClient.newHttpClient();
+		url = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/";
+		url += MAP_KEY;
+		url += "/VIIRS_SNPP_NRT/";
+		url += MIN_LONGITUDE + ",";
+		url += MIN_LATITUDE + ",";
+		url += MAX_LONGITUDE + ",";
+		url += MAX_LATITUDE;
+		url += "/1";
 	}
 
 	public String get() throws IOException, InterruptedException {
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
-		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		return response.body();
-	}
-
-	public String post(String json) throws IOException, InterruptedException {
-		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(json)).build();
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 		return response.body();
 	}
